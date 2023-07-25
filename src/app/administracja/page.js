b84bbecfb3e828081f253c5745e3bb8a8eb1e8a9
MilTech124 <Jaroslawmatusiak124@gmail.com>
2023-07-25 21:23:35 +0200

    add new fetch

---
+++ src/app/administracja/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex text-4xl justify-center items-center">
+      <a href="http://admin.stylmetal.pl/wp-admin/post.php?post=9&action=edit">Realizacje</a>
+    </div>
+  );
+}
+
+export default page;
